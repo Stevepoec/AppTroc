b42cbfb08fbc2ff2a97af0a9e028de6e1589ad0a
alexclergerie <alexphy24@gmail.com>
2023-02-08 14:47:51 +0100

    Add de la UtilisateurDAO

---
+++ UtilisateurDAO.cs
@@ -0,0 +1,14 @@
+namespace DAL;
+
+public class UtilisateurDAO
+{
+    public Guid ID_personne { get; set; }=Guid.NewGuid();
+    public string Nom {get; set;}
+    public string Prenom {get; set;}
+    public string mail {get; set;}
+    public string adresse {get; set;}
+    public string telephonne {get; set;}
+    public virtual ICollection<PretDAO> Prets { get; set; }=new HashSet<FilmDAO>();
+
+   
+}
