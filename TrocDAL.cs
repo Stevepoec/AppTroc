021f977bba0ee064a7e29ada88941c7bc6d1f797
steve <steve.auditeur@gmail.com>
2023-02-08 15:06:41 +0100

    Ajout trocDAL

--- TrocDAL.cs
+++ TrocDAL.cs
@@ -1,5 +1,16 @@
-﻿namespace AppTroc;
-public class Class1
+﻿using Microsoft.EntityFrameworkCore;
+namespace DAL
 {
+public partial class TrocDAL : IdentityDbContext<UtilisateurDAO,RoleDAO, Guid>{ 
+    public MaDal(){ this.Database.EnsureCreated();}
+protected override void OnConfiguring(DbContextOptionsBuilder builder){
+    base.OnConfiguring(builder);
 
-}
+builder.UseSqlServer("data source=.;initial catalog=Troc; integrated security=true;");}
+
+public DbSet<PretDAO> Prets { get; set; }                          
+public DbSet<ObjetDAO> Objets { get; set; }                          
+public DbSet<UtilisateurDAO> Utilisateurs { get; set; }                                                 
+public DbSet<PhotoDAO> Photos { get; set; }
+
+}}
