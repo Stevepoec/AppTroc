2fe9879b5a142203198550cdfb3109d17fff0c0e
Dom <contact@msform.com>
2023-02-08 15:33:48 +0100

    ajout PhotoDAO

--- PhotoDAO.cs
+++ PhotoDAO.cs
@@ -4,8 +4,12 @@ namespace DAL
 {
     public class PhotoDAO
     {
-        public Guid Id_Photo;
+        public Guid Id {get; set;}=Guid.NewGuid();
+       
         public string Photo_Name;
+
+        public virtual ObjetDAO Objet{get; set;}
+
 
     }
 }
